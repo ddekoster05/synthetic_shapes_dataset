c47fe32d00c9db554f99e2df5c7b72b958131523
Dominieq <d.j.de.koster@umail.leidenuniv.nl>
2026-03-07 13:33:28 +0100

    Objects and viewpoints are now sampled from parameters. Some uninformative views are not perfect yet

--- blender_thesis.py
+++ blender_thesis.py
@@ -1,67 +1,79 @@
 import bpy
 import os
+import numpy as np
 
 base_directory = os.path.dirname(os.path.abspath(__file__))
 output_directory = os.path.join(base_directory, "samples")
 
 classes = ['cube', 'pyramid', 'cylinder', 'cone', 'sphere', 'ring']
-sample_amount = 1
+sample_amount = 20
 
-# TO_DO: ADD PARAMETERS TO SAMPLE FROM!
 
 def create_object(object_type):
-    # TO_DO: MAKE PARAMETERS ABLE TO BE SAMPLED
     match object_type:
         case 'cube':
             # Cube
+            base_size = np.random.uniform(0.3,3)
             bpy.ops.mesh.primitive_cube_add(
-                size=2.0,
-                location=(0, 0, 0)
+                #size=2.0,
+                size=1,
+                location=(0, 0, 0),
+                scale=(base_size, base_size, base_size)
             )
         case 'pyramid':
+            # Pyramid
+            # Since cube and pyramid are 'linked classes', the sampling sizes and parameters should be identical,
+            # otherwise model shortcuts may occur.
+            base_size = np.random.uniform(0.3,3)
             bpy.ops.mesh.primitive_cone_add(
-                vertices =4,
+                vertices=4,
                 radius1=1,
-                depth=2,
+                #depth=2,
+                depth=1,
                 location=(0, 0, 0),
-                scale=(1,1,0.8)
+                scale=(base_size,base_size,np.random.uniform(0.3,3))
             )
         case 'cylinder':
             # Cylinder
             bpy.ops.mesh.primitive_cylinder_add(
-                # Vertices should stay constant!!!
-                vertices=32,
-                radius=1,
-                depth=2,
+                vertices=64,
+                #radius=1,
+                radius=np.random.uniform(0.3,2),
+                #depth=2,
+                depth=np.random.uniform(0.3,3),
                 location=(0, 0, 0)
             )
         case 'cone':
             # Cone
             bpy.ops.mesh.primitive_cone_add(
-                vertices=32,
-                radius1=1,
+                vertices=64,
+                #radius1=1,
+                radius1=np.random.uniform(0.3,2),
                 radius2=0,
-                depth=2,
+                #depth=2,
+                depth=np.random.uniform(0.3,3),
                 location=(0, 0, 0)
             )
         case 'sphere':
             # Sphere
             bpy.ops.mesh.primitive_uv_sphere_add(
-                radius=1,
+                #radius=1,
+                radius=np.random.uniform(0.3,2),
                 location=(0, 0, 0),
             )
         case 'ring':
             # Ring
             bpy.ops.mesh.primitive_torus_add(
-                major_radius=1,
-                minor_radius=0.3,
+               #major_radius=1,
+                major_radius=(np.random.uniform(1.2,2)),
+                #minor_radius=0.3,
+                minor_radius=np.random.uniform(0.3, 1),
                 location=(0, 0, 0)
             )
 
     # Store a reference to the created object
     sampled_object = bpy.context.active_object
 
-    # TO_DO: Make material properties able to be sampled
     # Create new material
     material = bpy.data.materials.new(name="MyMaterial")
 
@@ -69,31 +81,47 @@ def create_object(object_type):
     bsdf = material.node_tree.nodes["Principled BSDF"]
 
     # Set properties of the object
-    bsdf.inputs["Base Color"].default_value = (1, 0, 0, 1)
-    bsdf.inputs["Metallic"].default_value = 0.3
+    #bsdf.inputs["Base Color"].default_value = (1, 0, 0, 1)
+    bsdf.inputs["Base Color"].default_value = (np.random.uniform(0.0,1.0),
+                                               np.random.uniform(0.0,1.0),
+                                               np.random.uniform(0.0,1.0),
+                                               1)
+    bsdf.inputs["Metallic"].default_value = np.random.uniform(0.0,1.0)
 
     # Assign material to object
     sampled_object.data.materials.append(material)
 
     return sampled_object
 
-def create_camera_light(used_object):
-    # TO_DO: MAKE LIGHT PARAMETERS ABLE TO BE SAMPLED
+def create_camera_light(used_object, informative):
     # We create a random light source
     light_data = bpy.data.lights.new(name="Lamp", type='POINT')
     light_object = bpy.data.objects.new(name="Lamp", object_data=light_data)
     bpy.context.collection.objects.link(light_object)
 
     # Set properties of the light
-    light_object.location = (0, 0, 5)
-    light_data.energy = 1000
+    #light_object.location = (0, 0, 5)
+    # With this configuration, the source can never be in the object, but that also means it cannot be on top of it.
+    light_object.location = (np.random.uniform(2.5,5),
+                             np.random.uniform(2.5,5),
+                             np.random.uniform(1.0,5.0)
+                             )
+    #light_data.energy = 1000
+    light_data.energy = np.random.uniform(10,1000)
 
-    # TO_DO: ADD SAMPLE RANGES FOR BOTH AMBIGUOUS AND UNAMBIGUOUS VIEWS
     camera_data = bpy.data.cameras.new(name="Camera")
     camera_object = bpy.data.objects.new(name="Camera", object_data=camera_data)
     bpy.context.collection.objects.link(camera_object)
 
-    camera_object.location = (5, 5, 5)
+    if informative:
+        camera_object.location = (np.random.uniform(2,10),
+                                  np.random.uniform(2,10),
+                                  np.random.uniform(0,10)
+                                  )
+    else:
+        camera_object.location = (np.random.uniform(0, used_object.dimensions[0]),
+                                  np.random.uniform(0, used_object.dimensions[1]),
+                                  np.random.uniform(-3,-5))
 
     # This script aligns the camera to the object.
     constraint = camera_object.constraints.new(type='TRACK_TO')
@@ -101,13 +129,8 @@ def create_camera_light(used_object):
 
     return camera_object
 
-
-for class_name in classes:
-    # TO_DO: AUTOMATE CREATING SAMPLES FOR AMBIGUOUS AND UNAMBIGUOUS SHAPES
-
-    class_directory = os.path.join(output_directory, class_name)
-
-    for i in range(sample_amount):
+def generate_samples(class_name, file_location, amount, informative):
+    for i in range(amount):
         # By default, blender already contains a cube, camera and light in a scene.
         # Therefore, we must delete all existing objects first.
         bpy.ops.object.select_all(action='SELECT')
@@ -115,9 +138,28 @@ for class_name in classes:
 
         # Generate an object and a camera viewpoint.
         current_object = create_object(class_name)
-        generated_camera = create_camera_light(current_object)
+        generated_camera = create_camera_light(current_object, informative)
+
+        # Generate a string whether a view is informative, such that this can be used for identifying samples
+        if informative:
+            informative_string = 'informative'
+        else:
+            informative_string= 'uninformative'
 
         # Render the scene using the generated camera point
         bpy.context.scene.camera = generated_camera
-        bpy.context.scene.render.filepath = f"{class_directory}/{class_name}_{i}.png"
+        bpy.context.scene.render.filepath = f"{file_location}/{class_name}_{informative_string}_{i}.png"
         bpy.ops.render.render(write_still=True)
+
+def build_dataset():
+    for class_name in classes:
+        class_directory = os.path.join(output_directory, class_name)
+        if class_name == 'sphere' or class_name == 'ring':
+            generate_samples(class_name, class_directory, sample_amount, True)
+        else:
+            informative_directory = os.path.join(class_directory, 'informative')
+            generate_samples(class_name, informative_directory, sample_amount//2, True)
+            uninformative_directory = os.path.join(class_directory, 'uninformative')
+            generate_samples(class_name, uninformative_directory, sample_amount//2, False)
+
+build_dataset()
