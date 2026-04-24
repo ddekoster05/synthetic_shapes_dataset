a2054445e33caaa444e725fd767b59d1d491533b
Dominieq <d.j.de.koster@umail.leidenuniv.nl>
2026-04-24 15:55:47 +0200

    Added a new 3D architecture and a new gradcam script for comparison purposes

---
+++ 3D_CNN_simpler.py
@@ -0,0 +1,384 @@
+import os
+import random
+random.seed(42)
+
+from tqdm import tqdm
+from PIL import Image
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+from torchvision import transforms
+
+batch_size = 256
+num_classes = 6
+configuration = 2
+
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor()
+])
+
+# Locate the base directory
+base_directory = os.path.dirname(os.path.abspath(__file__))
+
+
+class PairDataset(Dataset):
+    """
+    This Dataset class provides pairs according to three possible configurations.
+    0: two informative views
+    1: one informative and one uninformative view
+    2: two uninformative views
+
+    For the unambiguous shapes, two informative views are used.
+    """
+    def __init__(self, root, informative_set, uninformative_set, configuration, transform=transform):
+        self.root = root
+        self.informative_set = informative_set
+        self.uninformative_set = uninformative_set
+        self.configuration = configuration
+        self.transform = transform
+
+        self.pairs = []
+        self.build_pairs()
+
+    def class_to_idx(self, class_name):
+        classes = ["cone", "cube", "cylinder", "pyramid", "ring", "sphere"]
+        return classes.index(class_name)
+
+    def build_pairs(self):
+        # This function builds pairs according to the passed configuration
+        for class_name in self.informative_set.keys():
+            # Unambiguous objects are always pairs of two informative views.
+            if class_name not in self.uninformative_set:
+                files = self.informative_set[class_name]
+
+                # Create pairs and store them with their label
+                for _ in range(len(files)):
+                    a, b = random.sample(files, 2)
+                    self.pairs.append((class_name, a, b))
+            else:
+                informative_files = self.informative_set[class_name]
+                uninformative_files = self.uninformative_set[class_name]
+
+                # Create pairs according to the passed configuration, and store them with their label
+                for _ in range(len(informative_files)):
+                    if self.configuration == 0:
+                        a = random.choice(informative_files)
+                        b = random.choice(informative_files)
+                        self.pairs.append((class_name, a, b))
+                    elif self.configuration == 1:
+                        a = random.choice(informative_files)
+                        b = random.choice(uninformative_files)
+                        self.pairs.append((class_name, a, b))
+                    else:
+                        a = random.choice(uninformative_files)
+                        b = random.choice(uninformative_files)
+                        self.pairs.append((class_name, a, b))
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, idx):
+        # Retrieve class name and two used files.
+        class_name, file1, file2 = self.pairs[idx]
+
+        # Retrieve images for unambiguous objects.
+        if class_name not in self.uninformative_set:
+            image1 = Image.open(os.path.join(self.root,class_name, file1)).convert("RGB")
+            image2 = Image.open(os.path.join(self.root,class_name, file2)).convert("RGB")
+        # Retrieve images for ambiguous objects.
+        else:
+            if self.configuration == 0:
+                image1 = Image.open(os.path.join(self.root, class_name, "informative", file1)).convert("RGB")
+                image2 = Image.open(os.path.join(self.root, class_name, "informative", file2)).convert("RGB")
+            elif self.configuration == 1:
+                image1 = Image.open(os.path.join(self.root, class_name, "informative", file1)).convert("RGB")
+                image2 = Image.open(os.path.join(self.root, class_name, "uninformative", file2)).convert("RGB")
+            else:
+                image1 = Image.open(os.path.join(self.root, class_name, "uninformative", file1)).convert("RGB")
+                image2 = Image.open(os.path.join(self.root, class_name, "uninformative", file2)).convert("RGB")
+
+        # Transform objects according to passed transform.
+        if self.transform:
+            image1 = self.transform(image1)
+            image2 = self.transform(image2)
+
+        # Stack both views, thus making the exemplar multidimensional.
+        x = torch.stack([image1, image2], dim=1)
+        label = self.class_to_idx(class_name)
+
+        return x, label
+
+
+def split_data():
+    """
+    This function splits the dataset into train and test sets for informative and uninformative views.
+    :return: two dictionaries, one containing the different sets for all informative views,
+    one containing the different sets for all uninformative views.
+    """
+    train_ratio = 0.8
+    validation_ratio = 0.1
+
+    # Retrieve all classes
+    classes = os.listdir(os.path.join(base_directory, "samples"))
+    informative_exemplars = {"train": {}, "validation": {}, "test": {}}
+    uninformative_exemplars = {"train": {}, "validation": {}, "test": {}}
+
+    for class_name in classes:
+        class_directory = os.path.join(base_directory, "samples", class_name)
+
+        # For unambigous shapes, only informative views are possible
+        if class_name == "ring" or class_name == "sphere":
+            informative_files = os.listdir(class_directory)
+            random.shuffle(informative_files)
+
+        else:
+            # Retrieve and shuffle informative and uninformative views.
+            uninformative_directory = os.path.join(class_directory, "uninformative")
+            informative_directory = os.path.join(class_directory, "informative")
+
+            uninformative_files = os.listdir(uninformative_directory)
+            random.shuffle(uninformative_files)
+
+            informative_files = os.listdir(informative_directory)
+            random.shuffle(informative_files)
+
+            # Split uninformative views in a training, validation and test set.
+            train_uninformative_exemplars = uninformative_files[
+                :int(len(uninformative_files) * train_ratio)
+            ]
+            validation_uninformative_exemplars = uninformative_files[
+                int(len(uninformative_files) * train_ratio):int(len(uninformative_files) * (train_ratio + validation_ratio))
+            ]
+            test_uninformative_exemplars = uninformative_files[
+                int(len(uninformative_files) * (train_ratio + validation_ratio)):
+            ]
+
+            # Store uninformative views in a dictionary according to the set and class they are part of.
+            train_dictionary_uninformative = uninformative_exemplars["train"]
+            train_dictionary_uninformative[class_name] = train_uninformative_exemplars
+            uninformative_exemplars["train"] = train_dictionary_uninformative
+
+            validation_dictionary_uninformative = uninformative_exemplars["validation"]
+            validation_dictionary_uninformative[class_name] = validation_uninformative_exemplars
+            uninformative_exemplars["validation"] = validation_dictionary_uninformative
+
+            test_dictionary_uninformative = uninformative_exemplars["test"]
+            test_dictionary_uninformative[class_name] = test_uninformative_exemplars
+            uninformative_exemplars["test"] = test_dictionary_uninformative
+
+        # Split informative views in a training, validation and test set.
+        train_informative_exemplars = informative_files[
+            :int(len(informative_files) * train_ratio)
+        ]
+        validation_informative_exemplars = informative_files[
+            int(len(informative_files) * train_ratio):int(len(informative_files) * (train_ratio + validation_ratio))
+        ]
+        test_informative_exemplars = informative_files[
+            int(len(informative_files) * (train_ratio + validation_ratio)):
+        ]
+
+        # Store informative views in a dictionary according to the set and class they are part of.
+        train_dictionary_informative = informative_exemplars["train"]
+        train_dictionary_informative[class_name] = train_informative_exemplars
+        informative_exemplars["train"] = train_dictionary_informative
+
+        validation_dictionary_informative = informative_exemplars["validation"]
+        validation_dictionary_informative[class_name] = validation_informative_exemplars
+        informative_exemplars["validation"] = validation_dictionary_informative
+
+        test_dictionary_informative = informative_exemplars["test"]
+        test_dictionary_informative[class_name] = test_informative_exemplars
+        informative_exemplars["test"] = test_dictionary_informative
+
+    # Return the dictionaries containing the informative and uninformative exemplars.
+    return informative_exemplars, uninformative_exemplars
+
+informative_exemplars, uninformative_exemplars = split_data()
+
+# Create datasets
+train_dataset = PairDataset(os.path.join(base_directory, "samples"),
+                            informative_exemplars["train"],
+                            uninformative_exemplars["train"],
+                            configuration
+                            )
+validation_dataset = PairDataset(os.path.join(base_directory, "samples"),
+                            informative_exemplars["validation"],
+                            uninformative_exemplars["validation"],
+                            configuration
+                            )
+test_dataset = PairDataset(os.path.join(base_directory, "samples"),
+                            informative_exemplars["test"],
+                            uninformative_exemplars["test"],
+                            configuration
+                            )
+
+# Load the datasets
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+def evaluate(model, test_loader, criterion, device):
+    """
+    Evaluate the CNN classifier on the validation set.
+
+    Args:
+        model (CNN): CNN classifier to evaluate.
+        test_loader (torch.utils.data.DataLoader): Data loader for the test set.
+        criterion (callable): Loss function to use for evaluation.
+        device (torch.device): Device to use for evaluation.
+
+    Returns:
+        float: Average loss on the test set.
+        float: Accuracy on the test set.
+    """
+    model.eval()  # Set model to evaluation mode
+
+    with torch.no_grad():
+        total_loss = 0.0
+        num_correct = 0
+        num_samples = 0
+
+        for inputs, labels in test_loader:
+            # Move inputs and labels to device
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # Compute the logits and loss
+            logits = model(inputs)
+            loss = criterion(logits, labels)
+            total_loss += loss.item()
+
+            # Compute the accuracy
+            _, predictions = torch.max(logits, dim=1)
+            num_correct += (predictions == labels).sum().item()
+            num_samples += len(inputs)
+
+    # Evaluate the model on the validation set
+    avg_loss = total_loss / len(test_loader)
+    accuracy = num_correct / num_samples
+
+    return avg_loss, accuracy
+
+
+def train(model, train_loader, val_loader, optimizer, criterion, device,
+          num_epochs):
+    """
+    Train the CNN classifer on the training set and evaluate it on the validation set every epoch.
+
+    Args:
+    model (CNN): CNN classifier to train.
+    train_loader (torch.utils.data.DataLoader): Data loader for the training set.
+    val_loader (torch.utils.data.DataLoader): Data loader for the validation set.
+    optimizer (torch.optim.Optimizer): Optimizer to use for training.
+    criterion (callable): Loss function to use for training.
+    device (torch.device): Device to use for training.
+    num_epochs (int): Number of epochs to train the model.
+    """
+
+    # Place the model on device
+    model = model.to(device)
+    for epoch in range(num_epochs):
+        model.train()  # Set model to training mode
+
+        with tqdm(total=len(train_loader),
+                  desc=f'Epoch {epoch + 1}/{num_epochs}',
+                  position=0,
+                  leave=True) as pbar:
+            for inputs, labels in train_loader:
+                # Move inputs and labels to device
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # Compute the logits and loss
+                logits = model(inputs)
+                # loss = torch.tensor(0)
+                loss = criterion(logits, labels)
+
+                loss.backward()
+
+                optimizer.step()
+                optimizer.zero_grad()
+
+                # Update the progress bar
+                pbar.update(1)
+                pbar.set_postfix(loss=loss.item())
+
+            # Compute average loss and accuracy on the validation dataset after one epoch
+            avg_loss, accuracy = evaluate(model, val_loader, criterion, device)
+            print(
+                f'Validation set: Average loss = {avg_loss:.4f}, Accuracy = {accuracy:.4f}'
+            )
+
+def test(model, test_loader, device):
+    """
+    Test the model accuracy per class.
+
+    """
+    model.eval()
+    classes = ["cone", "cube", "cylinder", "pyramid", "ring", "sphere"]
+
+    # Initialize variables
+    per_class_correct = {cls: 0 for cls in classes}
+    per_class_total = {cls: 0 for cls in classes}
+
+    num_samples = 0
+    num_correct = 0
+
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            # Move the inputs and labels to the device
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # Calculate predictions, and compare them to ground truth.
+            logits = model(inputs)
+            _, preds = torch.max(logits, dim=1)
+            num_correct += (preds == labels).sum().item()
+            num_samples += labels.size(0)
+
+            # Update per-class counters
+            for cls_idx, cls_name in enumerate(classes):
+                cls_mask = labels == cls_idx
+                per_class_correct[cls_name] += (preds[cls_mask] == labels[cls_mask]).sum().item()
+                per_class_total[cls_name] += cls_mask.sum().item()
+
+    # Calculate overall and per class accuracy
+    overall_accuracy = num_correct / num_samples
+    per_class_accuracy = {cls: per_class_correct[cls] / per_class_total[cls]
+                          for cls in classes}
+
+    # Report accuracy scores
+    print(f"Overall accuracy: {overall_accuracy:.4f}")
+    print("Per-class accuracy:")
+    for cls, acc in per_class_accuracy.items():
+        print(f"{cls}: {acc:.4f}")
+
+
+# Choose GPU if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Initialize the chosen model
+model = models.video.mc3_18(pretrained=True)
+
+# Freeze early parameters
+for name, param in model.named_parameters():
+    if "layer4" not in name and "fc" not in name:
+        param.requires_grad = False
+
+# Replace the final layer with a linear layer that maps to 6 nodes.
+model.fc = nn.Linear(model.fc.in_features, num_classes)
+
+# Setup optimizer, loss function, training loop
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+criterion = nn.CrossEntropyLoss()
+train(model, train_dataloader, validation_dataloader, optimizer, criterion,
+      device, num_epochs=10)
+
+# Save trained model once finished
+torch.save({'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict()}, 'model_multiview_new_2.ckpt')
+
+test(model, test_dataloader, device)
---
+++ gradcam_new.py
@@ -0,0 +1,334 @@
+import os
+
+import numpy as np
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torchvision import models
+from torchvision import transforms
+
+num_classes = 6
+model_type  = "3D"
+layer = 10
+
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.ToTensor(),
+])
+
+class_names = ["cone", "cube", "cylinder", "pyramid", "ring", "sphere"]
+
+# Locate the base directory
+base_directory = os.path.dirname(os.path.abspath(__file__))
+
+def prepare_2D(image):
+    """
+    Prepares the image for PyTorch.
+
+    : param image: an image
+    :return: a processed and transformed image
+    """
+    image = transform(image)
+    image = image.unsqueeze(0)
+
+    print(f"Prepared image shape: {image.shape}")
+
+    return image
+
+def prepare_3D(image1, image2):
+    """
+    Prepares two images for Pytorch.
+
+    :param image1: an image
+    :param image2: an image
+    :return:
+    """
+    # Prepare image 1
+    image1 = transform(image1)
+
+    # Prepare image 2
+    image2 = transform(image2)
+
+    # Stack the two images
+    prepared_images = torch.stack([image1, image2], dim=1)
+    # Add the batch dimension that is lost for single exemplars
+    prepared_images = prepared_images.unsqueeze(0)
+    print(f"Prepared images shape: {prepared_images.shape}")
+
+    return prepared_images
+
+def load_model(type):
+    """
+    Loads the passed model
+
+    :param type: type of the model to be loaded
+    :return: a loaded model
+    """
+    if type == "2D":
+        checkpoint = torch.load(os.path.join(base_directory, "model_baseline_backup.ckpt"))
+        model = models.alexnet()
+        model.classifier[6] = nn.Linear(4096, num_classes)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        #checkpoint = torch.load(os.path.join(base_directory, "model_multiview_2.ckpt"))
+        checkpoint = torch.load(os.path.join(base_directory, "model_multiview_new_2.ckpt"))
+        model = models.video.mc3_18(pretrained=True)
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+    model.eval()
+
+    return model
+
+def print_prediction(pred):
+    probs = torch.softmax(pred, dim=1)
+    pred_idx = torch.argmax(probs, dim=1).item()
+    confidence = probs[0, pred_idx].item()
+    class_name = class_names[pred_idx]
+    print(f"Prediction: {class_name} (class {pred_idx}) with confidence {confidence:.4f}")
+
+def forwards_hook(module, input, output):
+    """
+    Adapted from https://towardsdatascience.com/grad-cam-from-scratch-with-pytorch-hooks/
+
+    Parameters:
+            module (nn.Module): The module where the hook is applied.
+            input (tuple of Tensors): Input to the module.
+            output (Tensor): Output of the module."""
+    ...
+
+def backwards_hook(module, grad_in, grad_out):
+
+    """
+    Adapted from https://towardsdatascience.com/grad-cam-from-scratch-with-pytorch-hooks/
+
+    Parameters:
+            module (nn.Module): The module where the hook is applied.
+            grad_in (tuple of Tensors): Gradients w.r.t. the input of the module.
+            grad_out (tuple of Tensors): Gradients w.r.t. the output of the module."""
+    ...
+
+# Replace all in-place ReLU activations with out-of-place ones
+def replace_relu(model):
+
+    for name, child in model.named_children():
+        if isinstance(child, torch.nn.ReLU):
+            setattr(model, name, torch.nn.ReLU(inplace=False))
+            print(f"Replacing ReLU activation in layer: {name}")
+        else:
+            replace_relu(child)  # Recursively apply to submodules
+
+# List to store activations
+activations = []
+
+# Function to save activations
+def save_activations(module, input, output):
+    # Adapted from https://towardsdatascience.com/grad-cam-from-scratch-with-pytorch-hooks/
+    activations.append(output.detach().cpu().numpy().squeeze())
+
+# List to store gradients
+gradients = []
+
+# Function to save gradients
+def save_gradient(module, grad_in, grad_out):
+    # Adapted from https://towardsdatascience.com/grad-cam-from-scratch-with-pytorch-hooks/
+    gradients.append(grad_out[0].cpu().numpy().squeeze())
+
+def compute_heatmap(model_type, model, image, layer):
+    """
+    Adapted from https://towardsdatascience.com/grad-cam-from-scratch-with-pytorch-hooks/
+    A function to compute the GRADCAM heatmap
+
+    :param model: The model to be used
+    :param image: The image to be evaluated on
+    :param layer: The layer to be evaluated on. Only works for the 2D model,
+    as the 3D model collapses in temporal dimension after the first layer.
+    :return: A heatmap of the given image
+    """
+    activations.clear()
+    gradients.clear()
+
+    if model_type == "2D":
+        hook = model.features[layer].register_forward_hook(save_activations)
+    else:
+        layer = model.layer4[-1].conv2
+        hook = layer.register_forward_hook(save_activations)
+    prediction = model(image)
+    print("Logits:", prediction)
+    print_prediction(prediction)
+    hook.remove()
+
+    act_shape = np.shape(activations[0])
+    print(f"Shape of activations: {act_shape}")
+
+    # Register the backward hook on a convolutional layer
+    if model_type == "2D":
+        hook = model.features[layer].register_backward_hook(save_gradient)
+    else:
+        hook = layer.register_backward_hook(save_gradient)
+    # Forward pass
+    output = model(image)
+    # Pick the class with highest score
+    score = output[0].max()
+    # Backward pass from the score
+    score.backward()
+    # Remove the hook after use
+    hook.remove()
+
+    # Obtain shape of the gradients
+    grad_shape = np.shape(gradients[0])
+    print(f"Shape of gradients: {grad_shape}")
+    model.zero_grad()
+
+    # Aggregrate all gradients
+    if model_type == "3D":
+        # gradients: (C, T, H, W)
+        # activations: (C, T, H, W)
+
+        # 1. Global average pooling over T, H, W
+        weights = np.mean(gradients[0], axis=(1, 2, 3))  # (C,)
+
+        # 2. Weighted combination
+        cam = np.sum(
+            activations[0] * weights[:, np.newaxis, np.newaxis, np.newaxis],
+            axis=0
+        )  # (T, H, W)
+
+        # 3. Collapse temporal dimension
+        cam = np.mean(cam, axis=0)  # (H, W)
+
+    else:
+        weights = np.mean(gradients[0], axis=(1, 2))
+        cam = np.sum(
+            activations[0] * weights[:, np.newaxis, np.newaxis],
+            axis=0
+        )
+
+    # ReLU
+    cam = np.maximum(cam, 0)
+
+    return cam
+
+def upsampleHeatmap(model_type, relu_weighted_activations, image):
+    """
+    Adapted from https://towardsdatascience.com/grad-cam-from-scratch-with-pytorch-hooks/
+    A function to upsample the GRADCAM heatmap
+
+    :param relu_weighted_activations: the GRADCAM heatmap
+    :param image: The image that was used to create the GRADCAM heatmap
+    :return: The upsampled heatmap
+    """
+
+    if model_type == "2D":
+        # Upsample the heatmap to the original image size
+        upsampled_heatmap = cv2.resize(relu_weighted_activations,
+                                    (image.size(3), image.size(2)),
+                                    interpolation=cv2.INTER_LINEAR)
+    else:
+        upsampled_heatmap = cv2.resize(relu_weighted_activations,
+                                    (image.size(4), image.size(3)),
+                                    interpolation=cv2.INTER_LINEAR)
+    print(np.shape(upsampled_heatmap))
+
+    return upsampled_heatmap
+
+def display_images_2D(upsampled_heatmap, original_image, layer):
+    """
+    Show the upsampled heatmap and original image
+
+    :param upsampled_heatmap: the upsampled heatmap
+    :param original_image: the original image
+    :return: None
+    """
+    # Visualise the heatmap
+    fig, ax = plt.subplots(1, 2, figsize=(8, 8))
+
+    # Input image
+    resized_img = original_image.resize((224, 224))
+    ax[0].imshow(resized_img)
+    ax[0].axis("off")
+    ax[0].title.set_text('Original image')
+
+    # Edge map for the input image
+    edge_img = cv2.Canny(np.array(resized_img), 100, 200)
+    ax[1].imshow(255 - edge_img, alpha=0.5, cmap='gray')
+
+    # Overlay the heatmap
+    ax[1].imshow(upsampled_heatmap, alpha=0.5, cmap='coolwarm')
+    ax[1].axis("off")
+    ax[1].title.set_text(f'Heatmap at layer {int(layer/2) if layer != 0 else 1}')
+
+    plt.savefig("heatmap.png")
+    plt.show()
+
+def display_images_3D(upsampled_heatmap, original_image1, original_image2):
+    """
+    Show the upsampled heatmap and a combination of the original images
+
+    :param upsampled_heatmap: the upsampled heatmap
+    :param original_image1: the first original image
+    :param original_image2: the second original image
+    :return: None
+    """
+    # Visualise the heatmap
+    fig, ax = plt.subplots(1, 2, figsize=(8, 8))
+
+    #Input image
+    img1 = original_image1.resize((224, 224))
+    img2 = original_image2.resize((224, 224))
+
+    # Merge images
+    merged = (np.array(img1).astype(float) + np.array(img2).astype(float)) / 2
+    merged = merged.astype(np.uint8)
+    ax[0].imshow(merged)
+    ax[0].axis("off")
+    ax[0].title.set_text('Original image')
+
+    # Edge map for the input image
+    edge_img = cv2.Canny(merged, 100, 200)
+    ax[1].imshow(255 - edge_img, alpha=0.5, cmap='gray')
+
+    # Overlay the heatmap
+    ax[1].imshow(upsampled_heatmap, alpha=0.5, cmap='coolwarm')
+    ax[1].axis("off")
+    ax[1].title.set_text(f'Heatmap at layer 4, at the final residual block')
+
+    plt.savefig("heatmap.png")
+    plt.show()
+
+# Load the model of choice
+loaded_model = load_model(model_type)
+# Replace ReLU activations such that they don't switch
+replace_relu(loaded_model)
+
+# Prepare a test image
+#image_directory = os.path.join(base_directory, "real_images", "ring_1", "i_2.jpg")
+image_directory = os.path.join(base_directory, "edited_images", "ring_informative_2.png")
+original_image = Image.open(image_directory).convert("RGB")
+
+#image1_directory = os.path.join(base_directory, "real_images", "cylinder_1", "u_1.jpg")
+#image2_directory = os.path.join(base_directory, "real_images", "cylinder_1", "u_2.jpg")
+#image1_directory = os.path.join(base_directory, "samples", "cube", "informative", "cube_informative_17.png")
+#image2_directory = os.path.join(base_directory, "samples", "cube", "informative", "cube_informative_170.png")
+image1_directory = os.path.join(base_directory, "edited_images", "cylinder_uninformative_1.png")
+image2_directory = os.path.join(base_directory, "edited_images", "cylinder_uninformative_2.png")
+original_image1 = Image.open(image1_directory).convert("RGB")
+original_image2 = Image.open(image2_directory).convert("RGB")
+if model_type == "2D":
+    test_image = prepare_2D(original_image)
+else:
+    test_image = prepare_3D(original_image1, original_image2)
+
+# Obtain the relu weighted activations and upsample the heatmap.
+relu_weighted_activations = compute_heatmap(model_type, loaded_model, test_image, layer)
+upsampled_heatmap = upsampleHeatmap(model_type, relu_weighted_activations, test_image)
+
+# Display the heatmap and the original images
+if model_type == "2D":
+    display_images_2D(upsampled_heatmap, original_image, layer)
+else:
+    display_images_3D(upsampled_heatmap, original_image1, original_image2)
